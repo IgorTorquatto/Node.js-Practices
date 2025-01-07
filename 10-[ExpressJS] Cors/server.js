083794566d592f3bf780c1f0fd0653c190cbf754
Igor <igor.torquato.20@gmail.com>
2025-01-07 18:36:53 -0300

    add: cors

---
+++ 10-[ExpressJS] Cors/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql2');
+const cors = require('cors');
+
+const mysql_config = require('./mysql_config');
+
+//cors
+app.use(cors());
+
+//ligação com o banco
+const connection = mysql.createConnection(mysql_config);
+
+connection.connect(error=>{
+    if(error){
+        console.log(`Erro na conexão ao MySQL ${error.stack}`);
+        return;
+    }
+    console.log("Conexão bem sucedida");
+});
+
+app.get('/',(req,res)=>{
+    connection.query('SELECT * FROM tasks',(err,results,fields) => {
+        if(err){
+            console.log(err);
+            res.send('Erro ao obter a lista de tarefas');
+        } else {
+            res.send(results);
+        }
+    })
+});
+
+app.listen(3000,()=>{
+    console.log("Servidor executando");
+});
